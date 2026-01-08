62e378a86ea635a022e05e1b62f0b29624726dbd
Sandaru03 <sandarudilshan24@gmail.com>
2026-01-08 20:34:30 +0530

    Simple edit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -10,13 +9,12 @@ export default defineConfig({
   ],
 
   build: {
-    chunkSizeWarningLimit: 1000, // 1MB (warning remove)
-    
+    chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['axios'], // use karanne axios thiyenawanm
+          react: ['react', 'react-dom']
         }
       }
     }
